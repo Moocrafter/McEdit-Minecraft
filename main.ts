0417508d7224c135dd4c65fe569fa1420374a85f
Mooocraft <38020594+Mooocraft@users.noreply.github.com>
2019-03-21 16:09:15 -0400

    Updates.

--- main.ts
+++ main.ts
@@ -1,1 +1,19 @@
+let p1: positions.create(0, 0, 0)
+let p2: positions.create(0, 0, 0)
 
+player.onChat(":mcedit.p1", function () {
+    p1 = player.position()
+    p1 = positions.add(p1, positions.create(0, -1, 0))
+    player.say("p1 is " + p1)
+})
+
+
+player.onChat(":mcedit.p1", function () {
+    p1 = player.position()
+    p1 = positions.add(p1, positions.create(0, -1, 0))
+    player.say("p1 is " + p1)
+})
+
+player.onChat(":mcedit.fill", function (num1: numbers) {
+
+})
