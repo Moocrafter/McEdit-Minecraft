f56ef04cf4a401a2947121ea720f53c57a60214c
Mooocraft <38020594+Mooocraft@users.noreply.github.com>
2019-03-21 16:38:04 -0400

    Finished

--- main.ts
+++ main.ts
@@ -1,19 +1,19 @@
-let p1: positions.create(0, 0, 0)
-let p2: positions.create(0, 0, 0)
+let p1 = positions.create(0, -100, 0)
+let p2 = positions.create(0, -100, 0)
 
-player.onChat(":mcedit.p1", function () {
+player.onChat(":worldedit.p1", function () {
     p1 = player.position()
     p1 = positions.add(p1, positions.create(0, -1, 0))
     player.say("p1 is " + p1)
 })
 
 
-player.onChat(":mcedit.p1", function () {
-    p1 = player.position()
-    p1 = positions.add(p1, positions.create(0, -1, 0))
-    player.say("p1 is " + p1)
+player.onChat(":worldedit.p2", function () {
+    p2 = player.position()
+    p2 = positions.add(p2, positions.create(0, -1, 0))
+    player.say("p2 is " + p2)
 })
 
-player.onChat(":mcedit.fill", function (num1: numbers) {
-
+player.onChat(":worldedit.fill", function (inputId: number) {
+    blocks.fill(blocks.blockById(inputId), p1, p2)
 })
